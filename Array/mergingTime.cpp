14419e819f6258f215dcf3c5a52e8a4ef2d0a5ac
Surendra Kumar <32401892+comradesurendra@users.noreply.github.com>
2020-07-17 01:09:21 +0530

    first commit

---
+++ Array/mergingTime.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<pair<int,int>> mergeTime(pair<int,int> pa){
+    vector<pair<int,int>> res;
+
+}
+
+
+int main(){
+    int n,a,b;
+    cin>>n;
+    pair<int,int> pa[n];
+    for(int i=0;i<n;i++){
+        cin>>a>>b;
+        pa[i].first = a;
+        pa[i].second = b;
+    }
+
+    
+
+    // for(int i=0;i<n;i++){
+    //     cout<<pa[i].first<<" "<<pa[i].second<<endl;
+    // }
+
+    return 0;
+}
--- Pair/sortpair.cpp
+++ Pair/sortpair.cpp
@@ -2,9 +2,31 @@
 using namespace std;
 
 
-int main(){
-    pair<string,int> a = make_pair("Hello",12);
-    cout<<a.first <<" "<< a.second<<endl;
+void pairSort(int arr[],char chr[],int size){
+    pair<int,char> pr[size];
+    for(int i=0;i<size;i++){
+        pr[i].first = arr[i];
+        pr[i].second =chr[i];
+    }
+    sort(pr,pr+size);
 
+    for(int i=0;i<size;i++){
+        cout<<pr[i].first<<" "<<pr[i].second<<endl;
+    }
+}
+
+int main(){
+   int n;
+   cin>>n;
+   int arr[n];
+   char chr[n];
+   for(int i=0;i<n;i++){
+       cin>>arr[i];
+   }
+   for(int i=0;i<n;i++){
+       cin>>chr[i];
+   }
+
+   pairSort(arr,chr,n);
     return 0;
 }
